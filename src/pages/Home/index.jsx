502763de07bce515c247f4fe5287b5d5ca231d52
Viktoria <vikagoro2@gmail.com>
2020-04-09 21:01:35 +0300

    ka

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             <p className='p'>It helps developers to build beautiful and user-friendly web apps quickly and easily!</p>
             <button className='button'>Try in now</button>
           </div>
-          <img src='./public/img/imac.svg' />
+          <img src='/public/img/imac.png' />
         </div>
         <div className='container'>
           <div className='dot'></div>
@@ -68,6 +68,42 @@ const Home = () => {
           </div>
           <div className='figuree'>
             <img src='/public/img/figure-3.png'></img>
+          </div>
+        </div>
+        <div className='text-bottom'>
+          <div className='text3'>UX-Centered Design</div>
+          <div className='text3'>Supercharge App</div>
+          <div className='text3'>Mobile First</div>
+        </div>
+        <div className='text-bottom2'>
+          <div className='text4'>
+            Bespoke UX/UI design to enhance your app's user experience. Let AppKit take care of the design so you can
+            develop without design resources.
+          </div>
+          <div className='text4'>
+            AppKit provides developers with a ready-made front-end solution so you can concentrate on making your app’s
+            back-end robust and awesome. It empowers small teams to create big things.
+          </div>
+          <div className='text4'>
+            Built on the popular Bootstrap framework, so the design is “Mobile First” and fully responsive. For a
+            startup, building an HTML5 webapp which also works across mobile devices is the most cost-effective way to
+            get your product off the ground.
+          </div>
+        </div>
+        <div className='conteiner'>
+          <div className='conteiner2'>
+            <div className='title'>Many Happy Customers</div>
+          </div>
+          <div className='centr-img'>
+            <div className='profile'>
+              <img src='/public/img/profile-1.png'></img>
+
+              <p className='info'>
+                Testimonial goes here Donec felis odio, sagittis eu cursus ac, porttitor eu purus. In a bibendum dui.
+                Nullam id est sed felis rutrum tincidunt eu nec nisi morbi euismod semper neque sed lobortis.
+              </p>
+              <div className='jon'>@JohnK, San Francisco</div>
+            </div>
           </div>
         </div>
       </div>
