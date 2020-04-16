9312769a16b9b99caa70a06b64207a8250e60075
Viktoria Gorodetska <vikagoro3@gmail.com>
2020-04-16 16:48:44 +0300

    upd header

--- src/pages/Home/components/Header/index.jsx
+++ src/pages/Home/components/Header/index.jsx
@@ -4,36 +4,34 @@ import './index.styl'
 
 const Header = () => {
   return (
-    <div className='head'>
-      <Container className='Header'>
-        <div className='header-top'>
-          <div className='letter'>
-            <span className='span'>APP</span>KIT
-          </div>
-          <div className='menu'>
-            <div className='menu-item'>About</div>
-            <div className='menu-item'>Testimonials</div>
-            <div className='menu-item'>Features</div>
-            <div className='menu-item'>Team</div>
-            <div className='menu-item'>Pricing</div>
-            <div className='menu-item'>Contact</div>
-          </div>
+    <Container wrapperClassName='Header'>
+      <div className='header-top'>
+        <div className='letter'>
+          <span className='span'>APP</span>KIT
         </div>
-        <div className='header-bottom'>
-          <div className='menu-left'>
-            AppKit is the perfect front-end template for app developers.
-            <p className='p'>It helps developers to build beautiful and user-friendly web apps quickly and easily!</p>
-            <button className='button'>Try in now</button>
-          </div>
-          <img src='/public/img/imac.png' />
+        <div className='menu'>
+          <div className='menu-item'>About</div>
+          <div className='menu-item'>Testimonials</div>
+          <div className='menu-item'>Features</div>
+          <div className='menu-item'>Team</div>
+          <div className='menu-item'>Pricing</div>
+          <div className='menu-item'>Contact</div>
         </div>
-        <div className='container'>
-          <div className='dot-1' />
-          <div className='dot' />
-          <div className='dot' />
+      </div>
+      <div className='header-bottom'>
+        <div className='menu-left'>
+          AppKit is the perfect front-end template for app developers.
+          <p className='p'>It helps developers to build beautiful and user-friendly web apps quickly and easily!</p>
+          <button className='button'>Try in now</button>
         </div>
-      </Container>
-    </div>
+        <img src='/public/img/imac.png' />
+      </div>
+      <div className='container'>
+        <div className='dot' />
+        <div className='dot' />
+        <div className='dot' />
+      </div>
+    </Container>
   )
 }
 
